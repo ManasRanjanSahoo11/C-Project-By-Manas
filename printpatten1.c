1de8f466c33dbd1accd3316cfc7c1414901fd09e
Manas <manasranjansahoo971@gmail.com>
2023-10-16 19:53:08 +0530

    saved sucessfully

---
+++ printpatten1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+    int i,j;
+    char k;
+    for(i=1;i<=4;i++){
+        k='A';
+        for(j=1;j<=7;j++){
+            if(j>=i&&j<=8-i){
+                printf("%c",k);
+                j<4?k++:k--;
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
