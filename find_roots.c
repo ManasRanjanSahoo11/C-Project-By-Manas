2d63143d5f514e53109d4019a1850d04d1b41382
Manas <manasranjansahoo971@gmail.com>
2023-10-17 19:45:32 +0530

    Saved

---
+++ find_roots.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main(){
+    int a,b,c,d,r1,r2;
+    printf("Enter the three number :");
+    scanf("%d%d%d",&a,&b,&c);
+    d=b*b-4*a*c;
+    if(d>0){
+        r1=-(b+d)/2*a;
+        r2=-(b-d)/2*a;
+        printf("x=%d, x= %d\n",r1,r2);
+        printf("Real and distinct roots");
+    }
+    else if(b==0){
+        r1=-(b+d)/2*a;
+        r2=-(b-d)/2*a;
+        printf("x=%d, x= %d\n",r1,r2);
+        printf("Real and Equal roots");
+    }
+    else{
+        r1=-(b+d)/2*a;
+        r2=-(b-d)/2*a;
+        printf("x=%d, x= %d\n",r1,r2);
+        printf("Imaginary roots");
+    }
+
+}
